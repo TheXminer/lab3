eb4c7c1984103d7a0b347bfd0254a1ca5f5e9d58
Ilya <106274074+TheXminer@users.noreply.github.com>
2025-03-26 01:42:24 +0200

    Update lab3_sync.cpp
    Final version: collecting statistics

--- lab3_sync.cpp
+++ lab3_sync.cpp
@@ -7,6 +7,9 @@
 #include <functional>
 #include <map>
 #include <random>
+#include <cmath>
+#include <chrono>
+#include <atomic>
 
 enum Task_Status
 {
@@ -27,6 +30,8 @@ private:
     mutable read_write_lock m_rw_lock;
     task_queue_implementation m_tasks;
     unsigned int ID_counter = 0;
+    unsigned int editing_number = 0;
+    unsigned int total_length = 0;
 
 private:
     std::map<unsigned int, Task_Status> task_statuses;
@@ -39,6 +44,7 @@ public:
 public:
     inline size_t size();
     inline bool empty();
+    inline float get_average_length();
 
 public:
     template <typename... arguments>
@@ -65,6 +71,9 @@ public:
 
     bool working() const;
     bool working_unsafe() const;
+    float get_avrg_len();
+
+    void print_statistics();
 
 public:
     template <typename task_t, typename... arguments>
@@ -85,6 +94,10 @@ private:
 
     bool m_initialized = false;
     bool m_terminated = false;
+
+    std::atomic<int64_t> thread_wait_time{ 0 };
+    std::atomic<int64_t> thread_work_time{ 0 };
+    std::atomic<int> cycle_numbers{ 0 };
 };
 
 
@@ -103,6 +116,13 @@ inline bool task_queue<task_type_T>::empty()
 }
 
 template<typename task_type_T>
+inline float task_queue<task_type_T>::get_average_length()
+{
+    read_lock _(m_rw_lock);
+    return std::round((float)total_length / editing_number * 100) / 100;
+}
+
+template<typename task_type_T>
 inline void task_queue<task_type_T>::clear()
 {
     write_lock _(m_rw_lock);
@@ -118,7 +138,6 @@ inline bool task_queue<task_type_T>::add_status(unsigned int ID)
     write_lock _(task_rw_lock);
     if (task_statuses.count(ID) != 0)
         return false;
-
     std::cout << ID << ": " << "\033[31mNotStarted\033[0m" << std::endl;
     task_statuses[ID] = Task_Status::NotStarted;
     return true;
@@ -152,6 +171,8 @@ bool task_queue<task_type_T>::pop(unsigned int& ID, task_type_T& task) {
     if (m_tasks.empty()) {
         return false;
     }
+    editing_number++;
+    total_length += m_tasks.size();
     std::tie(ID, task) = std::move(m_tasks.front());
     m_tasks.pop();
     return true;
@@ -162,6 +183,8 @@ template <typename... arguments>
 unsigned int task_queue<task_type_T>::emplace(arguments&&... parameters) {
     write_lock _(m_rw_lock);
     ID_counter++;
+    editing_number++;
+    total_length += m_tasks.size();
     m_tasks.emplace(ID_counter, std::forward<arguments>(parameters)...);
     add_status(ID_counter);
     return ID_counter;
@@ -219,16 +242,22 @@ void thread_pool::routine()
                 task_accquired = m_tasks.pop(task_ID, task);
                 return m_terminated || task_accquired;
                 };
-
+            auto wait_beggin = std::chrono::high_resolution_clock::now();
             m_task_writer.wait(_, wait_condition);
+            auto wait_end = std::chrono::high_resolution_clock::now();
+            thread_wait_time.fetch_add(std::chrono::duration_cast<std::chrono::nanoseconds>(wait_end - wait_beggin).count());
         }
         if (m_terminated && !task_accquired)
             return;
         m_tasks.set_status(task_ID, Task_Status::InProgress);
+        auto task_beggin = std::chrono::high_resolution_clock::now();
         int a = task();
+        auto task_end = std::chrono::high_resolution_clock::now();
+        thread_work_time.fetch_add(std::chrono::duration_cast<std::chrono::nanoseconds>(task_end - task_beggin).count());
         write_lock _(result_rw_lock);
         task_results[task_ID] = a;
         m_tasks.set_status(task_ID, Task_Status::Completed);
+        cycle_numbers.fetch_add(1);
     }
 }
 
@@ -260,6 +289,25 @@ bool thread_pool::working_unsafe() const
     return !m_terminated && m_initialized;
 }
 
+float thread_pool::get_avrg_len()
+{
+    return m_tasks.get_average_length();
+}
+
+void thread_pool::print_statistics()
+{
+    std::cout << std::endl << std::endl << "Avrg len: " << get_avrg_len() << std::endl;
+    if(cycle_numbers != 0)
+    {
+        double avg_wait_time = (double)thread_wait_time.load() / cycle_numbers / 1e6;
+        double avg_work_time = (double)thread_work_time.load() / cycle_numbers / 1e6;
+
+        std::cout << "\nThread wait avg time: " << std::round(avg_wait_time * 100) / 100 << " ms\n";
+        std::cout << "Thread work avg time: " << std::round(avg_work_time * 100) / 100 << " ms\n";
+    }
+    std::cout << std::endl << std::endl;
+}
+
 Task_Status thread_pool::get_task_status(unsigned int ID)
 {
     return m_tasks.get_status(ID);
@@ -270,7 +318,7 @@ int thread_pool::get_task_results(unsigned int ID)
     read_lock _(result_rw_lock);
     auto it = task_results.find(ID);
     if (it == task_results.end()) {
-        throw std::runtime_error("Error: wrong task ID"); // Краще `std::runtime_error`
+        throw std::runtime_error("Error: wrong task ID");
     }
     return it->second;
 }
@@ -283,17 +331,25 @@ int thread_func() {
     return sleep_time;
 }
 
+void task_creation(thread_pool* pool, int task_number = INFINITY) {
+    for (int i = 0; i < task_number; i++)
+    {
+        std::uniform_int_distribution<int> distribution(1, 5);
+        std::this_thread::sleep_for(std::chrono::seconds(distribution(generator)));
+        pool->add_task(thread_func);
+    }
+}
+
 int main()
 {
     thread_pool pool;
     pool.initialize(4);
+
+    std::thread first_thread = std::thread(task_creation, &pool, 10);
+    std::thread second_thread = std::thread(task_creation, &pool, 10);
+    first_thread.join();
+    second_thread.join();
     
-    for(int i = 0; i < 25; i++)
-        pool.add_task(thread_func);
-    
-    std::this_thread::sleep_for(std::chrono::seconds(60));
-    std::cout << std::endl << std::endl << std::endl;
-    for (int i = 1; i < 26; i++) {
-        std::cout << i << ": " << pool.get_task_results(i) << std::endl;
-    }
+    pool.terminate();
+    pool.print_statistics();
 }
